5e62ecef49fdfe191fec014f780f4f7598115e77
LaurenSUni <111717811+LaurenSUni@users.noreply.github.com>
2022-09-19 13:31:59 +1000

    test2

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -46,6 +46,9 @@ public class CharacterMovement : MonoBehaviour
         playerAnimator.SetFloat("MovingSpeed", movementSqrMagnitude);
     }
 
+
+    //test2
+
     void FootstepAudio() {
     }
 }
